7df7dae759ae804a34888a424cb91239044612c0
David Amirkhanov <mail.websofter@gmail.com>
2025-05-07 12:13:50 +0300

    updt

--- app/userprofile/forms.py
+++ app/userprofile/forms.py
@@ -8,11 +8,11 @@ INPUT_CLASS = 'w-full my-4 py-4 px-6 rounded-xl bg-gray-100'
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'Your username',
+        'placeholder': 'Ваш логин',
         'class': INPUT_CLASS
     }))
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Your password',
+        'placeholder': 'Ваша пароль',
         'class': INPUT_CLASS
     }))
 
@@ -23,18 +23,18 @@ class SignupForm(UserCreationForm):
         fields = ('username', 'email', 'password1', 'password2')
     
     username = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'Your username',
+        'placeholder': 'Ваш логин',
         'class': INPUT_CLASS
     }))
     email = forms.CharField(widget=forms.EmailInput(attrs={
-        'placeholder': 'Your email address',
+        'placeholder': 'Ваш email',
         'class': INPUT_CLASS
     }))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Your password',
+        'placeholder': 'Ваша пароль',
         'class': INPUT_CLASS
     }))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Repeat password',
+        'placeholder': 'Повтор пароля',
         'class': INPUT_CLASS
     }))
